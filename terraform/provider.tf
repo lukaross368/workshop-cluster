e81fa00eda329939b6bfafeee50f005c5a818cf3
lukaross368 <luka.ross9@gmail.com>
2025-10-07 15:43:56 +0100

    added cluster

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,6 +4,18 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 4.12.0"
     }
+    azapi = {
+      source  = "azure/azapi"
+      version = "~>1.5"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "0.9.1"
+    }
   }
   required_version = ">= 1.6.6"
 }
--- terraform/resource_group.tf
+++ terraform/resource_group.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "resource_group" {
+resource "azurerm_resource_group" "rg" {
   name     = "workshop-cluster"
   location = "uksouth"
   tags     = local.tags
